4d563a882b67622c0ab3bc6813e9ebea70c18aeb
Usmon <usmooooon@gamil.com>
2021-09-15 16:23:57 +0500

    Added to firebase and added view binding

--- app/src/main/java/io/usmon/ebook/MainActivity.java
+++ app/src/main/java/io/usmon/ebook/MainActivity.java
@@ -1,14 +1,23 @@
 package io.usmon.ebook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.usmon.ebook.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+
     }
 }
